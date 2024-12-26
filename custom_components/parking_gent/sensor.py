57f84473ed886321bfb69280ac7d7d2838cb2f95
stijnpiron <stijnpiron@users.noreply.github.com>
2024-12-26 13:11:12 +0100

    refactor: move constants to separate file, remove unneeded sensor properties (#16)
    * refactor: move constants to separate file, remove unneeded sensor properties
    
    * test: update logging during tests

--- custom_components/parking_gent/sensor.py
+++ custom_components/parking_gent/sensor.py
@@ -1,63 +1,39 @@
 import logging
 import requests
-from datetime import timedelta
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-_LOGGER = logging.getLogger(__name__)
+from .constants import (
+    SCAN_INTERVAL,
+    FIELDS_GARAGE,
+    FIELDS_MOBI,
+    FIELDS_PR,
+    API_PARKING,
+    API_PR,
+    API_MOBI,
+)
 
+_LOGGER = logging.getLogger(__name__)
 
 """ requests only fetch a subset of relevant data, more documentation via the url. """
 """ the mobi endpoint is only used for 3 extra parking locations that are not available in the parking garage or p+r endpoints"""
 PARKING_API_URLS = [
     {
-        "documentationUrl":"https://data.stad.gent/explore/dataset/bezetting-parkeergarages-real-time/information/?sort=-occupation",
-        "url": "https://data.stad.gent/api/explore/v2.1/catalog/datasets/bezetting-parkeergarages-real-time/records?select=name,totalcapacity,availablecapacity,occupation,openingtimesdescription,isopennow,location,urllinkaddress,lastupdate&limit=100",
-        "mapping": {
-            "availableCapacity": "availablecapacity",
-            "isOpenNow": "isopennow",
-            "lastUpdate": "lastupdate",
-            "location": "location",
-            "name": "name",
-            "occupation": "occupation",
-            "openingTimes": "openingtimesdescription",
-            "totalCapacity": "totalcapacity",
-            "url": "urllinkaddress",
-        },
+        "documentationUrl": "https://data.stad.gent/explore/dataset/bezetting-parkeergarages-real-time/information/?sort=-occupation",
+        "url": API_PARKING,
+        "mapping": FIELDS_GARAGE,
     },
     {
-        "documentationUrl":"https://data.stad.gent/explore/dataset/real-time-bezetting-pr-gent/information/?sort=name",
-        "url": "https://data.stad.gent/api/explore/v2.1/catalog/datasets/real-time-bezetting-pr-gent/records?select=name,numberofspaces,availablespaces,occupation,openingtimesdescription,isopennow,location,urllinkaddress,lastupdate&limit=100",
-        "mapping": {
-            "availableCapacity": "availablespaces",
-            "isOpenNow": "isopennow",
-            "lastUpdate": "lastupdate",
-            "location": "location",
-            "name": "name",
-            "occupation": "occupation",
-            "openingTimes": "openingtimesdescription",
-            "totalCapacity": "numberofspaces",
-            "url": "urllinkaddress",
-        },
+        "documentationUrl": "https://data.stad.gent/explore/dataset/real-time-bezetting-pr-gent/information/?sort=name",
+        "url": API_PR,
+        "mapping": FIELDS_PR,
     },
     {
-        "documentationUrl":"https://data.stad.gent/explore/dataset/mobi-parkings/information/",
-        "url": "https://data.stad.gent/api/explore/v2.1/catalog/datasets/mobi-parkings/records?select=id_parking,totalcapacity,availablecapacity,occupation,openingtimesdescription,isopennow,location,urllinkaddress,lastupdate&where=totalcapacity > 0 and id_parking IN (\"Interparking Zuid\", \"Interparking Kouter\", \"Interparking Center\")&limit=100",
-        "mapping": {
-            "availableCapacity": "availablecapacity",
-            "isOpenNow": "isopennow",
-            "lastUpdate": "lastupdate",
-            "location": "location",
-            "name": "id_parking",
-            "occupation": "occupation",
-            "openingTimes": "openingtimesdescription",
-            "totalCapacity": "totalcapacity",
-            "url": "urllinkaddress",
-        },
+        "documentationUrl": "https://data.stad.gent/explore/dataset/mobi-parkings/information/",
+        "url": API_MOBI,
+        "mapping": FIELDS_MOBI,
     },
 ]
-
-SCAN_INTERVAL = timedelta(minutes=5)
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
@@ -90,11 +66,15 @@ class ParkingGentCoordinator(DataUpdateCoordinator):
         try:
             data = {}
             for api_config in PARKING_API_URLS:
-                response = await self.hass.async_add_executor_job(requests.get, api_config["url"])
+                response = await self.hass.async_add_executor_job(
+                    requests.get, api_config["url"]
+                )
                 response.raise_for_status()
                 api_data = response.json()
                 for record in api_data.get("results", []):
-                    normalized_record = self._normalize_record(record, api_config["mapping"])
+                    normalized_record = self._normalize_record(
+                        record, api_config["mapping"]
+                    )
                     parking_id = normalized_record["name"]
                     data[parking_id] = normalized_record
             return data
@@ -122,7 +102,7 @@ class ParkingSensor(SensorEntity):
     @property
     def icon(self):
         return self._icon
-    
+
     @property
     def name(self):
         """Return the name of the sensor."""
@@ -132,25 +112,15 @@ class ParkingSensor(SensorEntity):
     def state(self):
         """Return the state of the sensor (available capacity)."""
         return self.parking_data["availableCapacity"]
-    
+
+    @property
+    def available(self):
+        """Return True if the entity is available."""
+        return bool(self.parking_data.get("isOpenNow", False))
+
     @property
     def unit_of_measurement(self):
-        return "Spaces"
-    
-    @property
-    def device_state_attributes(self):
-        """Return additional attributes."""
-        return {
-            "isOpenNow": bool(self.parking_data["isOpenNow"]),
-            "lastUpdate": self.parking_data["lastUpdate"],
-            "location": self.parking_data["location"],
-            "latitude": self.parking_data["location"]["lat"],
-            "longitude": self.parking_data["location"]["lon"],
-            "occupation": self.parking_data["occupation"],
-            "openingTimes": self.parking_data["openingTimes"],
-            "totalCapacity": self.parking_data["totalCapacity"],
-            "url": self.parking_data["url"],
-        }
+        return "spaces"
 
     @property
     def extra_state_attributes(self):
